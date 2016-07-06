7bfeb02b706cf9530a92189360da4e2d4ee8483b
chinatoti <kbui@163.com>
2016-07-06 14:16:06 +0800

    bug

--- src/main/java/com/sumscope/optimus/definition/datacom/Row.java
+++ src/main/java/com/sumscope/optimus/definition/datacom/Row.java
@@ -1,21 +1,29 @@
 package com.sumscope.optimus.definition.datacom;
 
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by wenshuai.li on 2016/7/1.
  */
 public class Row {
+    private List<Data> fields = new ArrayList<>();
 
-    private Field field;
-    public Row(Field field){
-        this.field = field;
+    public Row(){
+    }
+
+
+    public Row addFieldData(Data data){
+        fields.add(data);
+        return this;
     }
 
     public int pack(ByteBuffer buffer) throws Exception {
         int size = 0;
-        Data data = field.getData();
-        size += data.encode(buffer).getSize();
+        for (Data data : fields){
+            size += data.encode(buffer).getSize();
+        }
         return size;
     }
 }
